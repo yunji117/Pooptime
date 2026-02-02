e08ba1c790be0c207503d0f0a1a9e16c962fda5f
yunji <yunji@yunjiui-MacBookAir.local>
2026-02-02 20:40:14 +0900

    feat: MyPage css edit

--- client/src/pages/Mypage.jsx
+++ client/src/pages/Mypage.jsx
@@ -21,38 +21,38 @@ function Mypage() {
         </button>
       </div>
       <div className="flex items-center pb-3 mb-3 ">
-        <img className="w-[40px]" src="./public/img/poopNameImg.svg" alt="" />
+        <img className="w-10" src="./public/img/poopNameImg.svg" alt="" />
         <h4 className="text-xm ml-4">{nickname} 님</h4>
       </div>
       <button
         type="button"
         onClick={() => navigate("/Mypage/info")}
-        className="flex w-full items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),_0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2"
+        className="flex w-full items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2"
       >
         <p className="text-sm">회원정보</p>
         <img src="./public/img/arrowRight.svg" alt="" />
       </button>
-      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),_0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
+      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
         <p className="text-sm">내가 맞춘 퀴즈</p>
         <img src="./public/img/arrowRight.svg" alt="" />
       </div>
-      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),_0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
+      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
         <p className="text-sm">내가 본 컨텐츠</p>
         <img src="./public/img/arrowRight.svg" alt="" />
       </div>
-      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),_0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
+      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
         <p className="text-sm">나의 즐겨찾기 컨텐츠</p>
         <img src="./public/img/arrowRight.svg" alt="" />
       </div>
-      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),_0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
+      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
         <p className="text-sm">내가 추천한 컨텐츠</p>
         <img src="./public/img/arrowRight.svg" alt="" />
       </div>
-      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),_0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
+      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
         <p className="text-sm">고객센터</p>
         <img src="./public/img/arrowRight.svg" alt="" />
       </div>
-      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),_0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
+      <div className="flex items-center justify-between shadow-[0_0_5px_0_rgba(0,0,0,0.1),0_0_1px_0_rgba(0,0,0,0.1)] p-3 mb-2">
         <p className="text-sm">개인정보 처리방침</p>
         <img src="./public/img/arrowRight.svg" alt="" />
       </div>
